c6141ddcef0ba7cafdf8f10be8103a75e45327da
sandeepa de silva <desilvasks@gmail.com>
2024-04-27 22:35:20 +0530

    add grid

--- my-raect-app/src/App.jsx
+++ my-raect-app/src/App.jsx
@@ -1,6 +1,7 @@
 // App.js
 import React from 'react';
 import './styles/Home.css';
+import APOD from './components/APOD';
 
 
 
@@ -8,18 +9,24 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col" style={{ marginLeft: '50px',padding: '100px', background: 'red',  width: '600px' }}>
-          1
+      <div className="col image-container">
+          <img src="https://apod.nasa.gov/apod/image/2404/20240408h14.jpg" alt="NASA Image" className="img-fluid" />
         </div>
         <div className="col">
           <div className="row" style={{ marginLeft: '10px' , padding: '100px', background: 'blue' ,width: '400px' }}>
             2
           </div>
-          <div className="row" style={{marginLeft: '10px' ,marginTop: '10px' , padding: '100px', background: 'gray' ,  width: '300px'}}>
+          <div className="row" style={{marginLeft: '10px' ,marginTop: '10px' , padding: '100px', background: 'gray' ,  width: '400px'}}>
             3
           </div>
         </div>
       </div>
+      <div className="row">
+      <div className="col" style={{ marginLeft: '100px',marginTop: '10px',padding: '100px', background: 'red',  width: '700px' }}>
+          4
+        </div>
+      </div>
+<APOD/>
     </div>
   );
 }
