0658270c3a732c525b7d027083c647dd8b93d872
sandeepa de silva <desilvasks@gmail.com>
2024-04-28 21:04:06 +0530

    added header and category

--- my-raect-app/src/App.jsx
+++ my-raect-app/src/App.jsx
@@ -2,32 +2,25 @@
 import React from 'react';
 import './styles/Home.css';
 import APOD from './components/APOD';
+import Header from './components/Header';
+import Catagory from './components/Catagory';
 
 
 
 function App() {
   return (
     <div className="container">
+      <Header/>
       <div className="mainImg">
       <APOD/>
+      <Catagory/>
       </div>
       <div className="row" style={{ marginTop: '100px'}}>
       <div className="col image-container">
           <img src="https://apod.nasa.gov/apod/image/2404/20240408h14.jpg" alt="NASA Image" className="img-fluid" />
+         
         </div>
-        <div className="col">
-          <div className="row" style={{ marginLeft: '10px' , padding: '100px', background: 'blue' ,width: '400px' }}>
-            2
-          </div>
-          <div className="row" style={{marginLeft: '10px' ,marginTop: '10px' , padding: '100px', background: 'gray' ,  width: '400px'}}>
-            3
-          </div>
-        </div>
-      </div>
-      <div className="row">
-      <div className="col" style={{ marginLeft: '100px',marginTop: '10px',padding: '100px', background: 'red',  width: '700px' }}>
-          4
-        </div>
+       
       </div>
     </div>
   );
