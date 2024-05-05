41ed1e913ad0fbdf00f3a136818a91d8ded42d77
sandeepa de silva <desilvasks@gmail.com>
2024-05-05 15:46:02 +0530

    Added responsive design to APOD

--- my-raect-app/src/components/APOD.jsx
+++ my-raect-app/src/components/APOD.jsx
@@ -1,7 +1,5 @@
-// components/APOD.js
 import React, { useEffect, useState } from 'react';
 import { fetchAPOD } from '../api/CommonAPI';
-import '../styles/APOD.css';
 
 const APOD = () => {
   const [apodData, setAPODData] = useState(null);
@@ -16,11 +14,25 @@ const APOD = () => {
   }, []);
 
   return (
-    <div className='apod'>
+    <div className="apod">
       {apodData && (
         <>
-          <h2>{apodData.title}</h2>
-          <img className='mainImg1' src={apodData.hdurl} alt={apodData.title} />
+          <h2 className="apod-title">{apodData.title}</h2>
+
+          {/* Responsive Image Handling with Max-Width Constraint */}
+          <picture>
+            <source
+              srcSet={`${apodData.url} 320w, ${apodData.hdurl} 1024w`}
+              media="(max-width: 768px)" // Target screens up to 768px wide
+            />
+            <img
+              className="mainImg1"
+              src={apodData.hdurl}
+              alt={apodData.title}
+              style={{ maxWidth: '100%' }} // Constrain image width to container
+            />
+          </picture>
+
           <p className="description">{apodData.explanation}</p>
         </>
       )}
