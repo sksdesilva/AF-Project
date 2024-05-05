5bced2440cbc27ebede321410717f06ebdd6f9e1
sandeepa de silva <desilvasks@gmail.com>
2024-05-05 15:46:17 +0530

    Added responsive design to category

--- my-raect-app/src/components/Catagory.jsx
+++ my-raect-app/src/components/Catagory.jsx
@@ -13,17 +13,9 @@ function Category() {
   };
 
   return (
-    <div className="container" style={{ marginLeft: '120px' }}>
+    <div className="container">
       <div className="row">
-        <div
-          className="col"
-          style={{
-            width: '50%',
-            height: '60vh',
-            backgroundColor: 'white',
-            position: 'relative',
-          }}
-        >
+        <div className="col">
           <img
             src="https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20190530011359.png?api_key=DEMO_KEY"
             style={{ width: '100%', height: '100%', objectFit: 'cover' }}
@@ -34,15 +26,7 @@ function Category() {
             <div className="overlay" style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '10px' }}>Earth resolution</div>
           )}
         </div>
-        <div
-          className="col"
-          style={{
-            width: '50%',
-            height: '60vh',
-            backgroundColor: 'white',
-            position: 'relative',
-          }}
-        >
+        <div className="col">
           <img
             src="http://apod.nasa.gov/apod/image/1310/velafilaments_jadescope_960.jpg"
             style={{ width: '100%', height: '100%', objectFit: 'cover' }}
@@ -55,16 +39,7 @@ function Category() {
         </div>
       </div>
       <div className="row">
-        <div
-          className="col"
-          style={{
-            marginTop: '10px',
-            width: '50%',
-            height: '60vh',
-            backgroundColor: 'white',
-            position: 'relative',
-          }}
-        >
+        <div className="col">
           <img
             src="https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/04102/opgs/edr/fcam/FLB_761645828EDR_F1060660FHAZ00302M_.JPG"
             style={{ width: '100%', height: '100%', objectFit: 'cover' }}
